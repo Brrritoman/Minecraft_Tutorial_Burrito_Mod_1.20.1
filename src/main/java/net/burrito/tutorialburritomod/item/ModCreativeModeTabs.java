f779946af3d232618da99206fba709f57f1d99ee
Brrritoman <a.h.skaggs05@gmail.com>
2025-03-20 13:07:46 -0500

    added data gen. added custom tags

---
+++ src/main/java/net/burrito/tutorialburritomod/item/ModCreativeModeTabs.java
@@ -0,0 +1,53 @@
+package net.burrito.tutorialburritomod.item;
+
+import net.burrito.tutorialburritomod.TutorialBurritoMod;
+import net.burrito.tutorialburritomod.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab>  CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialBurritoMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_BURRITO_TAB = CREATIVE_MODE_TABS.register("tutorial_burrito_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+                .title(Component.translatable("creativetab.tutorial_burrito_tab"))
+                    // adding to creative tab
+                .displayItems((pParameters, pOutput) -> {
+                    // adding modded items
+                    pOutput.accept(ModItems.SAPPHIRE.get());
+                    pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+
+                    pOutput.accept(ModItems.METAL_DETECTOR.get());
+
+                    pOutput.accept(ModItems.STRAWBERRY.get());
+
+                    pOutput.accept(ModItems.PINE_CONE.get());
+
+                    // adding vanilla items
+                    pOutput.accept(Items.DIAMOND);
+
+                    // adding modded blocks
+                    pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                    pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+                    pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
+                    pOutput.accept(ModBlocks.DEEPSlATE_SAPPHIRE_ORE.get());
+                    pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                    pOutput.accept(ModBlocks.END_SAPPHIRE_ORE.get());
+
+                    pOutput.accept(ModBlocks.SOUND_BLOCK.get());
+
+                })
+                .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
